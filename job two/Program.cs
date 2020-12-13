2f1c55306e7e7aca501f4495830b4b64a8d64b8d
SemenTsvolko <75906654+SemenTsvolko@users.noreply.github.com>
2020-12-13 18:54:33 +0300

    Add files via upload

---
+++ job two/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace job_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double w1;
+               double L = 0.7;
+               double m = 2;
+               double w = 4;
+               double a = w;
+               double b = (m * m) - 1;
+                w1 = (a / b);
+                Console.WriteLine("w1=" + w1);
+                double c = 2*w1;
+                double d = L;
+                double I1 = Math.Sqrt(c/d);
+                Console.WriteLine("I1=" + I1);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
